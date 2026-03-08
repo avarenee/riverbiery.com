7c18b64a785e9205df3a5098b52726ad463cc7f2
avarenee <riverbiery@gmail.com>
2026-03-08 16:48:13 -0400

    Add next.js rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ module.exports = {
     return [
       {
         source: '/projects/n-queens',
-        destination: 'https://n-queens-iota.vercel.app'
+        destination: 'https://n-queens-iota.vercel.app/projects/n-queens'
       },
       {
         source: '/projects/n-queens/:path*',
-        destination: 'https://n-queens-iota.vercel.app/:path*'
+        destination: 'https://n-queens-iota.vercel.app/projects/n-queens/:path*'
       }
     ]
   }
